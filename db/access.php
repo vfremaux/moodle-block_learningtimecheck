2e59322f50c88ff52cc472cbdc904f8dd983ee87
valery <valery.fremaux@gmail.com>
2016-10-18 12:19:04 +0200

    Automated sync from SVN

--- db/access.php
+++ db/access.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Checklist block caps.
+ * Learningtimecheck block caps.
  *
  * @package    block_learningtimecheck
  * @category   blocks
