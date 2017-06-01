9c9bea85748ce2b24a095aefbf79e9b489660876
valery <valery.fremaux@gmail.com>
2017-06-02 01:21:19 +0200

    fix travis compil in 32. Add paging for big audience

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2015071500;
-$plugin->requires = 2016051900;
+$plugin->requires = 2016120500;
 $plugin->component = 'block_learningtimecheck';
-$plugin->release  = '3.1.0 (Build: 2015071500)';
+$plugin->release  = '3.2.0 (Build: 2015071500)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('mod_learningtimecheck' => 2014011800); // Must have learningtimecheck activity module installed.
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
