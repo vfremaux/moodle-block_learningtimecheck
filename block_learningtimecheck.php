7097c2b18dc1aa12dd11e1bb4e59a5572ac78e19
valery <valery.fremaux@gmail.com>
2017-10-07 19:30:18 +0200

    Travis checks

--- block_learningtimecheck.php
+++ block_learningtimecheck.php
@@ -93,6 +93,8 @@ class block_learningtimecheck extends block_base {
         $viewallreports = has_capability('mod/learningtimecheck:viewreports', $context);
         $viewmenteereports = has_capability('mod/learningtimecheck:viewmenteereports', $context);
 
+        $allusers = array();
+
         if ($viewallreports || $viewmenteereports) {
             $orderby = 'ORDER BY firstname ASC';
             $ausers = false;
@@ -143,8 +145,10 @@ class block_learningtimecheck extends block_base {
 
                     $auser->longtimenosee = false;
                     if (!empty($this->config->longtimenosee)) {
-                        if ($maxlogstamp = $this->get_last_log_in_course($learningtimecheck->course, $auser->id)) {
-                            if ($maxlogstamp < (time() - $this->config->longtimenosee * 7 * DAYSECS)) {
+                        $params = array('userid' => $auser->id, 'courseid' => $learningtimecheck->course);
+                        $lastaccess = $DB->get_record('user_lastaccess', $params);
+                        if ($lastaccess) {
+                            if ($lastaccess->timeaccess < (time() - $this->config->longtimenosee * 7 * DAYSECS)) {
                                 $auser->longtimenosee = $this->config->longtimenosee;
                             }
                         } else {
